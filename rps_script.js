617653c16d00880fd8049ac0a3b984a7ed226643
Viktoria <wiktoriamitchell@yahoo.com>
2022-02-24 18:03:48 -0500

    added media queries

--- rps_script.js
+++ rps_script.js
@@ -1,49 +1,63 @@
-let myArray = ['rock', 'paper', 'scissors'];
+let result = 'Start';
+let humanSelection;
+let aiSelection;
+let humanScore = 0;
+let aiScore = 0;
 
-function computerPlay() {
-    return myArray[Math.floor(Math.random() * myArray.length)];
+function playGame(humanInput) {
+  humanSelection = humanInput.textContent;
+  aiSelection = getAiSelection();
+  let result = calcResult(humanSelection, aiSelection);
+  document.getElementById("vs-text").innerText = humanSelection + ' VS ' + aiSelection;
+  showScore();
 }
 
-// ask the user if they choose rock paper or scissors
-//make the output case-insensitive
-//if user answer is same as computer game, alert "TIE GAME"
-//if user is rock and computer is scissors, alert you win
-// if user is paper and computer is rock, alert you win
-//if user is scissors and computer is paper, alert you win
-
-function playRound() {
-let userAnswer = prompt("Rock Paper or Scissors?", "");
-let playerSelection = userAnswer.toLowerCase();
-computerSelection = computerPlay();
-console.log(playerSelection,computerSelection);
-
-if (playerSelection == computerSelection) {
-    alert("Tie Game!");
-} else if (playerSelection == "rock" && computerSelection == "scissors") {
-    alert("You win! Rock beats Scissors");
-} else if 
-(playerSelection == "paper" && computerSelection == "rock") {
-    alert("You win! Paper beats Rock");
-} else if 
-(playerSelection == "scissors" && computerSelection == "paper") {
-    alert("You win! Scissors beats Paper");
-} else if 
-(playerSelection == "scissors" && computerSelection == "rock"){
-    alert("You lost. Rock beats Scissors");
-} else if 
-(playerSelection == "paper" && computerSelection == "scissors"){
-    alert("You lost. Scissors beats Paper");
-} else if 
-(playerSelection == "rock" && computerSelection == "paper"){
-    alert("You lost. Paper beats Rock");
-} else {
-    alert("Choose Rock Paper or Scissors");
-}
+function getAiSelection() {
+  const options = ['rock', 'paper', 'scissors'];
+  let aiSelector = options[Math.floor(Math.random()*options.length)];
+  return aiSelector;
 }
 
+function calcResult(human, ai) {
+  
+  if (human === ai) {
+    result = 'Draw'
+    return;
+  } else if ((human === 'rock' && ai === 'scissors') || 
+  (human === 'paper' && ai === 'rock') || (human === 'scissors' && ai === 'paper')) {
+    humanScore += 1;
 
-playRound();
-playRound();
-playRound();
-playRound();
-playRound();
+    if (humanScore == 5) {
+      result = "You are the winner!";
+      endgame();
+    } else {
+      result = 'You win!'
+      return;
+    }
+
+  } else {
+    aiScore += 1;
+    if (aiScore == 5) {
+      result = 'Computer is the Winner'
+      endgame();
+    } else {
+      result = ' You lose!'
+      return;
+    }
+  }
+}
+
+function showScore() {
+  document.getElementById("playerscore").innerText = humanScore;
+  document.getElementById("aiscore").innerText = aiScore;
+  document.getElementById("result").innerText = result;
+
+}
+
+function endgame() {
+  const ele = document.getElementById('startgame');
+  ele.remove();
+  document.getElementById("endgame").style.display = "block";
+  showScore();
+}
+
